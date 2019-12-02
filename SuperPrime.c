96c5c3471c0236053e27539f472b51e95fe64384
Coisini-L <1029285434@qq.com>
2019-12-02 23:01:47 +0800

    homework

--- SuperPrime.c
+++ SuperPrime.c
@@ -1,2 +1,59 @@
-//è¶…çº§ç´ æ•°ï¼šå®ƒæœ¬èº«ï¼Œå„ä½æ•°å­—çš„å’Œï¼Œå„ä½æ•°å­—çš„å¹³æ–¹å’Œï¼Œéƒ½æ˜¯ç´ æ•°ã€‚
-//æ±‚ 100~10000 å†…çš„æ‰€æœ‰è¶…çº§ç´ æ•°ï¼ŒåŠå®ƒä»¬çš„å¹³å‡æ•°ã€‚
+#include <stdio.h>
+
+int PJ(int x[],int j);
+
+int main (){
+	int x[10000];
+	int i,j=0;
+	for (i=100;i<=10000;i++){
+		int s,t=0;
+		for (s=2;s<i;s++){
+			if (i%s==0) t++;
+		}
+		if (t==0){
+			int n1,n2,n3,n4;
+			n1=i/1000;
+			n2=(i/100)-10*n1;
+			n3=(i/10)-100*n1-10*n2;
+			n4=i-1000*n1-100*n2-10*n3;
+			int sum=n1+n2+n3+n4;
+			int n=0;
+			for (s=1;s<=sum;s++){
+				if (sum%s==0) n++;
+			}
+			if (n==2){
+				int sum2=n1*n1+n2*n2+n3*n3+n4*n4;
+				int m=0;
+				for (s=1;s<=sum2;s++){
+					if (sum2%s==0) m++;
+				}
+				if (m==2){
+					x[j]=i;
+					j++;
+				}
+			}
+			else continue;
+		}
+		else continue;
+	}
+	for (i=0;i<j;i++){
+		printf("%d\n",x[i]);
+	}
+	float pj=PJ(x,j);
+	printf ("%f",pj);
+	return 0;
+}
+
+int PJ(int x[],int j){
+	int i;
+	int sum=0;
+	for (i=0;i<j;i++){
+		sum+=x[i];
+	}
+	float pj;
+    pj=sum/j;
+	return pj;
+}
+
+//³¬¼¶ËØÊı£ºËü±¾Éí£¬¸÷Î»Êı×ÖµÄºÍ£¬¸÷Î»Êı×ÖµÄÆ½·½ºÍ£¬¶¼ÊÇËØÊı¡£
+//Çó 100~10000 ÄÚµÄËùÓĞ³¬¼¶ËØÊı£¬¼°ËüÃÇµÄÆ½¾ùÊı¡£
